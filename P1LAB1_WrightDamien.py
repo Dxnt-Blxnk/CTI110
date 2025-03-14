7a8e80b2bd12e3d773584a5c4b27603f796d4145
Dxnt-Blxnk <wrightd2067@student.faytechcc.edu>
2025-03-13 20:00:14 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_WrightDamien.py
@@ -0,0 +1,14 @@
+# Damien Wright
+# 11 March 2025
+# P1LAB1
+# This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+
+
+# Prompt the user to enter their first name and assign it to a variable.
+first_name = input("Enter your first name: ")
+
+# Prompt the user to enter their last name and assign it to a variable.
+last_name = input("Enter your last name: ")
+
+# Display a welcome message
+print("Welcome to this course,", first_name, last_name,"!")
